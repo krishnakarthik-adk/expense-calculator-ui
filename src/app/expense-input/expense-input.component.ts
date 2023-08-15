39f3605b3750a2d17b1d6bb46d3d8849a9278722
Krishna Karthik <krishnakarthik.adk@gmail.com>
2023-08-15 14:24:16 +0530

    Improved styling fr tables. Add and delete rows for the expense input.

--- src/app/expense-input/expense-input.component.ts
+++ src/app/expense-input/expense-input.component.ts
@@ -1,17 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule, NgForm } from '@angular/forms';
+import { ExpenseInput } from './expense-input';
 
 @Component({
   selector: 'ec-expense-input',
   templateUrl: './expense-input.component.html',
   styleUrls: ['./expense-input.component.css']
 })
-export class ExpenseInputComponent {
+export class ExpenseInputComponent implements OnInit{
   pageTitle: string = 'Expense Input';
 
-  item: string = '';
-  amount: string = '';
-  expenseCategory:string = 'None';
-  comments: string = '';
+  expenseInput: ExpenseInput  =  {
+    item: '',
+    amount: '',
+    expenseCategory: 'None',
+    notes: ''
+  }; 
 
+  dynamicExpenseArray: ExpenseInput [] = [];  
+
+  addRow(): void {
+    this.dynamicExpenseArray.push({
+      item: '',
+      amount: '',
+      expenseCategory: 'None',
+      notes: ''});           
+  }
+
+  deleteRow(index:number) {
+    this.dynamicExpenseArray.splice(index);
+  }
+
+  ngOnInit(): void {
+    this.dynamicExpenseArray.push(this.expenseInput);
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form.valid);
+  }
+  
 }
---
+++ src/app/expense-input/expense-input.ts
@@ -0,0 +1,6 @@
+export interface ExpenseInput {
+    item: string;
+    amount: string;
+    expenseCategory: string;
+    notes: string;
+}
