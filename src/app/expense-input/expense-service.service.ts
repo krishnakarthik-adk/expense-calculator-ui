b1b7055a86d077286c8fe0b771d9f2bfd974f54d
Krishna Karthik <krishnakarthik.adk@gmail.com>
2023-09-05 22:41:51 +0530

    Changes to include AWS host URL in record my expenses screen. Title case display for expenses.

--- src/app/expense-input/expense-service.service.ts
+++ src/app/expense-input/expense-service.service.ts
@@ -6,10 +6,14 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class ExpenseService {   
+export class ExpenseService {
+  
+  hostUrl: string = 'http://ec2-35-180-66-174.eu-west-3.compute.amazonaws.com:8080';
 
-  selectionOptionUrl: string = '/api/expensecalculator/v1/getExpenseRecordSelectOptions';
-  expenseFormSubmitUrl: string = '/api/expensecalculator/v1/saveExpenseRecords?dateOfExpense='; // dateOfExpense to be included
+  // selectionOptionUrl: string = '/api/expensecalculator/v1/getExpenseRecordSelectOptions'; // For local dev
+  selectionOptionUrl: string = this.hostUrl + '/api/expensecalculator/v1/getExpenseRecordSelectOptions'; // For AWS
+
+  expenseFormSubmitUrl: string = this.hostUrl + '/api/expensecalculator/v1/saveExpenseRecords' + '?dateOfExpense=';
 
   constructor(private http: HttpClient) { }
 
@@ -18,7 +22,7 @@ export class ExpenseService {
     // return of( expenseFormData); // fake. We're just returning back the receive data for test
   }
 
-  getExpenseRecordSelectOptions(): Observable<any> {
+  getExpenseRecordSelectOptions(): Observable<any> {    
       // return of(['None','EMI','Groceries','Veggies and Fruits','Home Maintenance','Utility Bills','Shopping','Fuel','Vehicle Maintenance','Savings','Personal','Medical','Miscellaneous']);
       return this.http.get(this.selectionOptionUrl); // To do
   }
